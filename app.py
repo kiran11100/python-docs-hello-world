07a44cb346ac3f6650ee9c334a7409f8475111e5
kiran11100 <49929220+kiran11100@users.noreply.github.com>
2021-11-16 20:20:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 import hashlib
 app = Flask(__name__)
-
+app.debug=False
 @app.route("/")
 def hello():
-    surname="anumala"
-    result=hashlib.sha256(surname.encode())
-    return "Hello,".result
+    surname = "anumala"
+    result = hashlib.sha256(surname.encode('utf-8')).hexdigest()
+    data = "Hello   " + result
+    return data
